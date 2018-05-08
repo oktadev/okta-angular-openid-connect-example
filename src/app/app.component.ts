5e069a3c379d4af6d9171785217f635336161c78
Matt Raible <matt.raible@okta.com>
2018-05-07 20:29:18 -0600

    Polishing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,13 @@ export class AppComponent {
   title = 'app';
   
   constructor(private oauthService: OAuthService) {
-      this.oauthService.redirectUri = window.location.origin;
-      this.oauthService.clientId = '0oaexo3i16rvMNvOz0h7';
-      this.oauthService.scope = 'openid profile email';
-      this.oauthService.issuer = 'https://dev-158606.oktapreview.com/oauth2/default';
-      this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.redirectUri = window.location.origin;
+    this.oauthService.clientId = '0oaexo3i16rvMNvOz0h7';
+    this.oauthService.scope = 'openid profile email';
+    this.oauthService.issuer = 'https://dev-158606.oktapreview.com/oauth2/default';
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
 
-      // Load Discovery Document and then try to login the user
-      this.oauthService.loadDiscoveryDocumentAndTryLogin();
-    }
+    // Load Discovery Document and then try to login the user
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
 }
